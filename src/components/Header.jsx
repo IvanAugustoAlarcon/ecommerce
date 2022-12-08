f749a3748fb7d587e87f260fb9470e6d3b581fc1
Ivan Alarcon <augusto.iah@gmail.com>
2022-12-07 18:28:33 -0600

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <nav className='navbar navbar-expand-lg bg-light'>
+          <div className='container-fluid'>
+            <h5 className='navbar-brand'>Ecommer</h5>
+            <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+              <span className='navbar-toggler-icon' />
+            </button>
+            <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+              <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+                <li className='nav-item'>
+                  <Link className='nav-link' to='/'>Home</Link>
+                </li>
+                <li className='nav-item'>
+                  <Link to='/login' className='nav-link'>Login</Link>
+                </li>
+                <li className='nav-item'>
+                  <Link to='/signup' className='nav-link'>Signup</Link>
+                </li>
+              </ul>
+              <form className='d-flex' role='search'>
+                <input className='form-control me-2' type='search' placeholder='Search' aria-label='Search' />
+              </form>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  )
+}
+// onChange={() => { handleSearch(event.target.value) }}
+export default Header
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import Error404 from '../pages/Error404'
 import Home from '../pages/Home'
+import Login from '../pages/Login'
 import Product from '../pages/Product'
+import Signup from '../pages/Signup'
 
 const RoutesIndex = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
       <Route path='/product/:id' element={<Product />} />
       <Route path='*' element={<Error404 />} />
     </Routes>
